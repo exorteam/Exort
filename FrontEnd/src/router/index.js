936df14856d218fc7b831f1d7c39539f599a541a
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-07-03 19:51:07 +0800

    create_component

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AssociaMemManage from '../components/association_member_management/member_management._table'
+import AppliManagement from '../components/association_member_management/application_management_table'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/association_management',
+      name: 'AssociaMemManage',
+      component: AssociaMemManage
+    },
+    {
+      path:'/application_management',
+      name:'AppliManagement',
+      component:AppliManagement
     }
   ]
 })
