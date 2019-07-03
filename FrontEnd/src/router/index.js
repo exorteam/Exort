a393d60cebe2cf76b214d0796c232110499197ea
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-07-03 17:35:39 +0800

    association member management

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
+import AssociaMemManage from '../components/association_member_management/member_management._table'
+import AppliManagement from '../components/association_member_management/application_management_table'
 
 Vue.use(Router);
 
 export default new Router({
-  mode : 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/association_member_management',
+      name: 'AssociaMemManage',
+      component: AssociaMemManage
+    },
+    {
+      path:'/application_management',
+      name:'AppliManagement',
+      component:AppliManagement
     }
   ]
 })
