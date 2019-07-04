02841ca8770ffc518106696e67c8a9b8d3076114
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-07-04 14:14:42 +0800

    add entity

---
+++ associationmanager/src/main/java/exort/associationmanager/entity/AssociationApplication.java
@@ -0,0 +1,17 @@
+package main.java.exort.associationmanager.entity;
+
+import java.util.*;
+import org.springframework.data.annotation.Id;
+import lombok.Data;
+
+@Data
+public class AssociationApplication{
+    
+    @Id
+    private int id;
+    private int user_id;
+    private Association asso;
+    private Date apply_time;
+    private int state; //unhandled,accepted,refused,canceled
+    
+}
