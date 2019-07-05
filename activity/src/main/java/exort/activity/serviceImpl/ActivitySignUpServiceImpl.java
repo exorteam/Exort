a2c246b87e78e6c3018d3120e87c93a12a420637
rsp <1747836307@qq.com>
2019-07-05 17:24:06 +0800

    update details
    Former-commit-id: 8fb08bec12bb7e815e0fc0b11294cefda9eebcda

--- activity/src/main/java/exort/activity/serviceImpl/ActivitySignUpServiceImpl.java
+++ activity/src/main/java/exort/activity/serviceImpl/ActivitySignUpServiceImpl.java
@@ -7,7 +7,6 @@ import exort.activity.service.ActivitySignUpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
