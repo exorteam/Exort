1d7fc7760070cb6ae8aeb621547996599a2bd679
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-07-09 10:04:55 +0800

    add files

---
+++ association_manager/src/main/java/exort/associationmanager/entity/AssociationApplicationFilterParams.java
@@ -0,0 +1,16 @@
+package exort.associationmanager.entity;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class AssociationApplicationFilterParams {
+
+	private Int id
+	private Association asso;
+
+
+	private Integer state;
+
+}
