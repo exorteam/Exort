3f44ce69bb10cf90a5e30067c3f15b7d11ddce63
MrHate <dgyhateyou@sjtu.edu.cn>
2019-07-11 20:51:01 +0800

    complete tests on UserService

--- api_server/src/test/java/exort/apiserver/UserServiceTest.java
+++ api_server/src/test/java/exort/apiserver/UserServiceTest.java
@@ -9,8 +9,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import exort.apiserver.component.SecurityUtil;
 import exort.apiserver.entity.UserInfo;
 import exort.apiserver.repository.UserRepository;
 import exort.apiserver.service.UserService;
@@ -23,6 +25,8 @@ public class UserServiceTest {
 	private UserService service;
 	@Autowired
 	private UserRepository repository;
+	@Autowired
+	private SecurityUtil securityUtil;
 
 	private final int USER_TYPE_TEST = -10;
 
@@ -32,21 +36,23 @@ public class UserServiceTest {
 	}
 
 	@Test
-	public void createTestUserThenGetAndRemove(){
+	public void testCRUD(){
 		final String username = UUID.randomUUID().toString();
-		final String password = UUID.randomUUID().toString();
+		String password = UUID.randomUUID().toString();
 
 		UserInfo info = new UserInfo();
 		info.setType(USER_TYPE_TEST);
 		info.setUsername(username);
 		info.setPassword(password);
 		
-		int id = service.createUser(info);
+		final int id = service.createUser(info);
 		Assert.assertNotEquals(id,-1);
-
 		UserInfo info2 = service.getUserById(id);
 		Assert.assertEquals(info,info2);
 
+		password = UUID.randomUUID().toString();
+		info.setPassword(password);
+		service.updateUser(info);
 		UserInfo info3 = service.getUserByUsername(info.getUsername());
 		Assert.assertEquals(info,info3);
 
@@ -57,37 +63,18 @@ public class UserServiceTest {
 		Assert.assertNull(info5);
 	}
 
-	//@Test
-	//public void authTest(){
-	//    ArrayList<UserInfo> users = new ArrayList<UserInfo>();
-	//    int[] userTypes = {0,0,0,1};
+	@Test
+	public void testCurrentUser(){
+		UserInfo info = new UserInfo();
+		info.setUsername(UUID.randomUUID().toString());
+		info.setPassword(UUID.randomUUID().toString());
+		info.setType(USER_TYPE_TEST);
+		info.setId(service.createUser(info));
 
-	//    // 1. Inexistent username
-	//    // 2. Wrong password
-	//    // 3. log in as normal user
-	//    // 4. log in as admin
-	//    for(int i=0;i<4;i++){
-	//        UserInfo user = new UserInfo();
-	//        user.setUsername(UUID.randomUUID().toString());
-	//        user.setPassword(UUID.randomUUID().toString());
-	//        user.setType(userTypes[i]);
-	//        user.setId(service.createUser(user));
-	//        users.add(user);
-	//    }
-
-	//    int res1 = service.auth(users.get(0).getUsername()+UUID.randomUUID().toString().substring(0,5),
-	//            users.get(0).getPassword());
-	//    Assert.assertEquals(res1,-1);
-
-	//    int res2 = service.auth(users.get(1).getUsername(),
-	//            users.get(1).getPassword()+UUID.randomUUID().toString().substring(0,5));
-	//    Assert.assertEquals(res2,-1);
-
-	//    int res3 = service.auth(users.get(2).getUsername(),users.get(2).getPassword());
-	//    Assert.assertEquals(res3,0);
-
-	//    int res4 = service.auth(users.get(3).getUsername(),users.get(3).getPassword());
-	//    Assert.assertEquals(res4,1);
-	//}
+		securityUtil.logInAs(info.getUsername());
+		UserInfo info2 = service.getCurrentUser();
+		Assert.assertEquals(info,info2);
+	}
+		
 }
 
