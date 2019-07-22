f6ac148675a8385e0a04c281312e4997b52e9b74
MrHate <dgyhateyou@sjtu.edu.cn>
2019-07-22 15:49:11 +0800

    jwt filter

--- api_server/src/main/java/exort/apiserver/controller/UserInfoController.java
+++ api_server/src/main/java/exort/apiserver/controller/UserInfoController.java
@@ -3,8 +3,8 @@ package exort.apiserver.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,18 +20,18 @@ public class UserInfoController {
 	@Autowired
 	private UserInfoService service;
 
-	@GetMapping("/info/{id}")
-	public UserInfo getUserInfoById(@PathVariable("id") int id){
+	@GetMapping("/info")
+	public UserInfo getUserInfoById(@RequestAttribute("id") int id){
 		return service.getUserInfo(id);
 	}
 
-	@PostMapping("/info/{id}")
-	public UserInfo updateUserInfoById(@PathVariable("id") int id,@RequestBody UserInfo info){
+	@PostMapping("/info")
+	public UserInfo updateUserInfoById(@RequestAttribute("id") int id,@RequestBody UserInfo info){
 		return service.updateUserInfo(id,info);
 	}
 
-	@PatchMapping("/info/{id}")
-	public boolean disableUserById(@PathVariable("id") int id,@RequestParam boolean disabled){
+	@PatchMapping("/info")
+	public boolean disableUserById(@RequestAttribute("id") int id,@RequestParam boolean disabled){
 		return service.disableUser(id,disabled);
 	}
 }
