6b21e1b5f4c0226fbe72f4e5e2bbecbf64c3e589
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-07-22 13:09:45 +0800

    modify

--- common/src/main/java/exort/api/http/review/entity/Callback.java
+++ common/src/main/java/exort/api/http/review/entity/Callback.java
@@ -1,9 +1,11 @@
 package exort.api.http.review.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,5 +23,11 @@ public class Callback {
         this.endpoint = endpoint;
         this.url = url;
         this.description = description;
+    }
+    public Callback(String type, String event, String endpoint, String url) {
+        this.type = type;
+        this.event = event;
+        this.endpoint = endpoint;
+        this.url = url;
     }
 }
