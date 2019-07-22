a71288495ef7f238035ea95879200d8dc88a0216
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-07-22 22:00:04 +0800

    finish

--- api_server/src/main/java/exort/apiserver/serviceimpl/AssociationServiceImpl.java
+++ api_server/src/main/java/exort/apiserver/serviceimpl/AssociationServiceImpl.java
@@ -1,13 +1,17 @@
 package exort.apiserver.serviceimpl;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import exort.apiserver.service.AssociationManagerService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.minidev.json.JSONUtil;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-import org.json.JSONObject;
+import com.google.gson.*;
+
+
 
 import java.util.List;
 
@@ -17,6 +21,16 @@ public class AssociationServiceImpl implements AssociationManagerService {
     public static class PatchAssociationInfo {
         private String description;
         private String type;
+        public PatchAssociationInfo(String _body){
+            byte[] b = _body.getBytes();
+            Base64 base64 = new Base64();
+            b = base64.decode(b);
+            String s = new String(b);
+            Gson gson = new Gson();
+            PatchAssociationInfo association = gson.fromJson(s, PatchAssociationInfo.class);//对于javabean直接给出class实例
+            this.description = association.description;
+            this.type = association.type;
+        }
     }
 
     @Data
@@ -24,6 +38,17 @@ public class AssociationServiceImpl implements AssociationManagerService {
         private String userId;
         private String event;
         private Application application;
+        public ApplicationAssociationInfo(String _body){
+            byte[] b = _body.getBytes();
+            Base64 base64 = new Base64();
+            b = base64.decode(b);
+            String s = new String(b);
+            Gson gson = new Gson();
+            ApplicationAssociationInfo association = gson.fromJson(s, ApplicationAssociationInfo.class);//对于javabean直接给出class实例
+            this.userId = association.userId;
+            this.event = association.event;
+            this.application = association.application;
+        }
 
     }
 
@@ -58,16 +83,6 @@ public class AssociationServiceImpl implements AssociationManagerService {
         private List<String> tags;
         private Integer state; //0 blocked,1 active
         private String reason;
-        public Association(String _body){
-
-            byte[] b = _body.getBytes();
-            Base64 base64 = new Base64();
-            b = base64.decode(b);
-            String s = new String(b);
-            Association association  = JSONObject.parseObject(str, Invoice.class);
-
-
-        }
     }
 
     @Data
@@ -93,12 +108,35 @@ public class AssociationServiceImpl implements AssociationManagerService {
         private String description;
         private List<String> tags;
         private  String logo;
+        public AssociationInfo(String _body){
+            byte[] b = _body.getBytes();
+            Base64 base64 = new Base64();
+            b = base64.decode(b);
+            String s = new String(b);
+            Gson gson = new Gson();
+            AssociationInfo association = gson.fromJson(s, AssociationInfo.class);//对于javabean直接给出class实例
+            this.name = association.name;
+            this.description = association.description;
+            this.logo = association.logo;
+            this.tags = association.tags;
+        }
     }
     @Data
     public class AssociationFilterParams {
         private String keyword;
         private List<String> tags;
         private Integer state;
+        public AssociationFilterParams(String _body){
+            byte[] b = _body.getBytes();
+            Base64 base64 = new Base64();
+            b = base64.decode(b);
+            String s = new String(b);
+            Gson gson = new Gson();
+            AssociationFilterParams association = gson.fromJson(s, AssociationFilterParams.class);//对于javabean直接给出class实例
+            this.keyword = association.keyword;
+            this.tags = association.tags;
+            this.state = association.state;
+        }
     }
 
 
@@ -110,7 +148,7 @@ public class AssociationServiceImpl implements AssociationManagerService {
     public Response<Association> createAssociation(String _body){
         HttpMethod method = HttpMethod.POST;
         headers.setContentType(MediaType.APPLICATION_JSON);
-        AssociationInfo requestBody = AssociationInfo(_body);
+        AssociationInfo requestBody = new AssociationInfo(_body);
         HttpEntity<AssociationInfo> requestEntity = new HttpEntity<>(requestBody,headers);
         ResponseEntity<Response> response = restTemplate.exchange("http://202.120.40.8:30728/associations",method,requestEntity,Response.class);
 
@@ -120,7 +158,7 @@ public class AssociationServiceImpl implements AssociationManagerService {
     public Response<AssociationList> listAssociations(String _body, Integer pageNum, Integer pageSize){
         HttpMethod method = HttpMethod.GET;
         headers.setContentType(MediaType.APPLICATION_JSON);
-        AssociationFilterParams requestBody = AssociationFilterParams(_body);
+        AssociationFilterParams requestBody = new AssociationFilterParams(_body);
         HttpEntity<AssociationFilterParams> requestEntity = new HttpEntity<>(requestBody,headers);
         ResponseEntity<Response> response = restTemplate.exchange("http://202.120.40.8:30728/associations?pageNum="+pageNum.toString()+"&pageSize="+pageSize.toString(),method,requestEntity,Response.class);
 
@@ -146,7 +184,7 @@ public class AssociationServiceImpl implements AssociationManagerService {
     public Response<Association> editAssociation(String assoId, String _body){
         HttpMethod method = HttpMethod.PUT;
         headers.setContentType(MediaType.APPLICATION_JSON);
-        AssociationInfo requestBody = AssociationInfo(_body);
+        AssociationInfo requestBody = new AssociationInfo(_body);
         HttpEntity<AssociationInfo> requestEntity = new HttpEntity<>(requestBody,headers);
         ResponseEntity<Response> response = restTemplate.exchange("http://202.120.40.8:30728/associations/"+assoId,method,requestEntity,Response.class);
         return response.getBody();
@@ -155,7 +193,7 @@ public class AssociationServiceImpl implements AssociationManagerService {
     public Response<Object> patchAssociation(String assoId, String _body){
         HttpMethod method = HttpMethod.PATCH;
         headers.setContentType(MediaType.APPLICATION_JSON);
-        PatchAssociationInfo requestBody = PatchAssociationInfo(_body);
+        PatchAssociationInfo requestBody = new PatchAssociationInfo(_body);
         HttpEntity<PatchAssociationInfo> requestEntity = new HttpEntity<>(requestBody,headers);
         ResponseEntity<Response> response = restTemplate.exchange("http://202.120.40.8:30728/associations/"+assoId,method,requestEntity,Response.class);
         return response.getBody();
@@ -165,7 +203,7 @@ public class AssociationServiceImpl implements AssociationManagerService {
     public Response<Object> handleAsoociationApplication(String _body){
         HttpMethod method = HttpMethod.POST;
         headers.setContentType(MediaType.APPLICATION_JSON);
-        ApplicationAssociationInfo requestBody = ApplicationAssociationInfo(_body);
+        ApplicationAssociationInfo requestBody = new ApplicationAssociationInfo(_body);
         HttpEntity<ApplicationAssociationInfo> requestEntity = new HttpEntity<>(requestBody,headers);
         ResponseEntity<Response> response = restTemplate.exchange("http://202.120.40.8:30728/callback",method,requestEntity,Response.class);
         return response.getBody();
