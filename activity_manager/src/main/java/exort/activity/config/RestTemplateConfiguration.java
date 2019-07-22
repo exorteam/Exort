183df4ec64bfdeae8cb9104bbf5dd5b2c3828654
rsp <1747836307@qq.com>
2019-07-22 10:34:29 +0800

    update pom

---
+++ activity_manager/src/main/java/exort/activity/config/RestTemplateConfiguration.java
@@ -0,0 +1,20 @@
+package exort.activity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(1000);
+        requestFactory.setReadTimeout(1000);
+
+        return new RestTemplate(requestFactory);
+    }
+
+}
