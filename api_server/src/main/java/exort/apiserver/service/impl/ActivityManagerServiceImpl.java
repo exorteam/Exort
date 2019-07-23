b16d0bb11a2a9535795b3d74599730e45dc346fa
rsp <1747836307@qq.com>
2019-07-23 16:52:29 +0800

    update restTemplate source

--- api_server/src/main/java/exort/apiserver/service/impl/ActivityManagerServiceImpl.java
+++ api_server/src/main/java/exort/apiserver/service/impl/ActivityManagerServiceImpl.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import exort.api.http.common.RestTemplate;
+// import org.springframework.web.client.RestTemplate;
 
 import exort.apiserver.service.ActivityManagerService;
 
@@ -17,102 +18,124 @@ public class ActivityManagerServiceImpl implements ActivityManagerService {
 
     private RestTemplate rt = new RestTemplate();
 
-    public Response createNewActivity(Activity activity){
+    public Response createNewActivity(Activity activity) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.POST;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Activity> requestEntity = new HttpEntity<>(activity, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities",method,requestEntity, Response.class);
+        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities", method, requestEntity,
+                Response.class);
         return response.getBody();
     }
 
-    public Response updateActivity(Activity activity, String activityid){
+    public Response updateActivity(Activity activity, String activityid) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.PUT;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Activity> requestEntity = new HttpEntity<>(activity, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/"+ activityid, method,requestEntity, Response.class);
+        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/" + activityid, method,
+                requestEntity, Response.class);
         return response.getBody();
     }
 
-    public Response getActivities(Select select, int pagesize, int pagenum, int sortby){
+    public Response getActivities(Select select, int pagesize, int pagenum, int sortby) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.GET;
         headers.setContentType(MediaType.APPLICATION_JSON);
-           HttpEntity<Select> requestEntity = new HttpEntity<>(select, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities"+"?pagesize="+Integer.toString(pagesize) +"&pagenum="+Integer.toString(pagenum)+"&sortby="+Integer.toString(sortby), method, requestEntity, Response.class);
+        HttpEntity<Select> requestEntity = new HttpEntity<>(select, headers);
+        ResponseEntity<Response> response = rt.exchange(
+                "http://202.120.40.8:30727/activities" + "?pagesize=" + Integer.toString(pagesize) + "&pagenum="
+                        + Integer.toString(pagenum) + "&sortby=" + Integer.toString(sortby),
+                method, requestEntity, Response.class);
         return response.getBody();
     }
 
-    public Response publishActivity(String activityid, String type){
+    public Response publishActivity(String activityid, String type) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.PATCH;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> requestEntity = new HttpEntity<>(type, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/"+ activityid,method,requestEntity, Response.class);
+        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/" + activityid, method,
+                requestEntity, Response.class);
         return response.getBody();
     }
 
-    public Response addParticipants(String activityid, List<Integer> participantIds){
+    public Response addParticipants(String activityid, List<Integer> participantIds) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.POST;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<List<Integer>> requestEntity = new HttpEntity<>(participantIds, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/"+activityid+"/participants",method,requestEntity, Response.class);
+        ResponseEntity<Response> response = rt.exchange(
+                "http://202.120.40.8:30727/activities/" + activityid + "/participants", method, requestEntity,
+                Response.class);
         return response.getBody();
     }
 
-    public Response addRealParticipants(String activityid, List<Integer> realParticipantIds){
+    public Response addRealParticipants(String activityid, List<Integer> realParticipantIds) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.POST;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<List<Integer>> requestEntity = new HttpEntity<>(realParticipantIds, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/"+activityid+"/realparticipants",method,requestEntity, Response.class);
+        ResponseEntity<Response> response = rt.exchange(
+                "http://202.120.40.8:30727/activities/" + activityid + "/realparticipants", method, requestEntity,
+                Response.class);
         return response.getBody();
     }
 
-    public Response deleteParticipants(String activityid, List<Integer> participantIds){
+    public Response deleteParticipants(String activityid, List<Integer> participantIds) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.DELETE;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<List<Integer>> requestEntity = new HttpEntity<>(participantIds, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/"+activityid+"/participants",method,requestEntity, Response.class);
+        ResponseEntity<Response> response = rt.exchange(
+                "http://202.120.40.8:30727/activities/" + activityid + "/participants", method, requestEntity,
+                Response.class);
         return response.getBody();
     }
 
-    public Response getActivityParticipants(int pagesize, int pagenum, String activityid, List<Integer> participantIds){
+    public Response getActivityParticipants(int pagesize, int pagenum, String activityid,
+            List<Integer> participantIds) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.GET;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<List<Integer>> requestEntity = new HttpEntity<>(participantIds, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/"+activityid+"/participants"+"?pagesize="+Integer.toString(pagesize) +"&pagenum="+Integer.toString(pagenum), method,requestEntity,Response.class);
+        ResponseEntity<Response> response = rt.exchange(
+                "http://202.120.40.8:30727/activities/" + activityid + "/participants" + "?pagesize="
+                        + Integer.toString(pagesize) + "&pagenum=" + Integer.toString(pagenum),
+                method, requestEntity, Response.class);
         return response.getBody();
     }
 
-    public Response getActivityRealParticipants(int pagesize, int pagenum, String activityid, List<Integer> realParticipantIds){
+    public Response getActivityRealParticipants(int pagesize, int pagenum, String activityid,
+            List<Integer> realParticipantIds) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.GET;
         headers.setContentType(MediaType.APPLICATION_JSON);
-           HttpEntity<List<Integer>> requestEntity = new HttpEntity<>(realParticipantIds, headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities/"+activityid+"/realparticipants"+"?pagesize="+Integer.toString(pagesize) +"&pagenum="+Integer.toString(pagenum),method,requestEntity, Response.class);
+        HttpEntity<List<Integer>> requestEntity = new HttpEntity<>(realParticipantIds, headers);
+        ResponseEntity<Response> response = rt.exchange(
+                "http://202.120.40.8:30727/activities/" + activityid + "/realparticipants" + "?pagesize="
+                        + Integer.toString(pagesize) + "&pagenum=" + Integer.toString(pagenum),
+                method, requestEntity, Response.class);
         return response.getBody();
     }
 
-    public Response acceptSignup(Operation operation){
+    public Response acceptSignup(Operation operation) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.POST;
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Operation> requestEntity = new HttpEntity<>(operation,headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/callback/acceptsignup",method,requestEntity, Response.class);
+        HttpEntity<Operation> requestEntity = new HttpEntity<>(operation, headers);
+        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/callback/acceptsignup", method,
+                requestEntity, Response.class);
         return response.getBody();
     }
 
-    public Response getActivity(String activityid){
+    public Response getActivity(String activityid) {
         HttpHeaders headers = new HttpHeaders();
         HttpMethod method = HttpMethod.GET;
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> requestEntity = new HttpEntity<>(headers);
-        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities"+activityid,method,requestEntity, Response.class);
+        ResponseEntity<Response> response = rt.exchange("http://202.120.40.8:30727/activities" + activityid, method,
+                requestEntity, Response.class);
         return response.getBody();
     }
 }
