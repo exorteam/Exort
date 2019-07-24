62d0ed130be892109bb6cd94bb0c8b7e76e48e31
Ice Cola <icecola2018@gmail.com>
2019-07-24 16:23:05 +0800

    修复RestTemplate无法解析泛型的bug

--- common/src/main/java/exort/api/http/common/ResponseType.java
+++
@@ -1,6 +0,0 @@
-package exort.api.http.common;
-
-import exort.api.http.common.entity.ApiResponse;
-import org.springframework.core.ParameterizedTypeReference;
-
-public class ResponseType<T> extends ParameterizedTypeReference<ApiResponse<T>> { }
