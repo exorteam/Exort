208502dc752f4afe5e8e8f381a8907d39b386a22
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-07-24 15:42:07 +0800

    修改navbar

--- web_client/src/components/admin_nav/nav.vue
+++ web_client/src/components/admin_nav/nav.vue
@@ -3,55 +3,24 @@
         <div class="layout-logo"></div>
         <div class="layout-nav">
             <Menu mode="horizontal" theme="dark" active-name="1" style="width: 100%;child-align: right">
-                <Submenu name="2">
-                    <template slot="title">
-                        <Icon type="ios-stats"/>
-                        通知
-                    </template>
-                    <MenuItem v-if="admin==='1'" name="3-1" disabled>Manager</MenuItem>
-                    <MenuItem v-if="admin==='0'" name="3-1" disabled>User</MenuItem>
-                    <Divider style="margin: 0"/>
-                    <MenuItem name="3-1" :to="{name:'ShoppingCart'}">My Shopping Cart</MenuItem>
-                    <MenuItem name="3-2" :to="{name:'AllOrders'}">My Orders</MenuItem>
-                    <Divider style="margin: 0"/>
-                    <MenuItem name="3-3">Account Setting</MenuItem>
-                    <Divider style="margin: 0"/>
-                    <MenuItem name="3-4"><a @click="quit">Logout</a></MenuItem>
-                </Submenu>
-                <Submenu name="3">
-                    <template slot="title">
-                        <Icon type="ios-stats"/>
-                        用户信息
-                    </template>
-                    <MenuItem v-if="admin==='1'" name="3-1" disabled>Manager</MenuItem>
-                    <MenuItem v-if="admin==='0'" name="3-1" disabled>User</MenuItem>
-                    <Divider style="margin: 0"/>
-                    <MenuItem name="3-1" :to="{name:'ShoppingCart'}">My Shopping Cart</MenuItem>
-                    <MenuItem name="3-2" :to="{name:'AllOrders'}">My Orders</MenuItem>
-                    <Divider style="margin: 0"/>
-                    <MenuItem name="3-3">Account Setting</MenuItem>
-                    <Divider style="margin: 0"/>
-                    <MenuItem name="3-4"><a @click="quit">Logout</a></MenuItem>
-                </Submenu>
+                <noticebar></noticebar>
+                <userbar></userbar>
             </Menu>
         </div>
     </div>
 </template>
 
 <script>
+    import noticebar from './notice'
+    import userbar from './user'
+
     export default {
-        name:"adminnav",
+        name: "adminnav",
+        components: {noticebar, userbar},
         data() {
-            return {
-                admin: 0
-            };
+            return {};
         },
-        methods: {
-            quit() {
-
-            },
-
-        },
+        methods: {},
     }
 </script>
 
@@ -72,6 +41,7 @@
         width: 420px;
         margin: 0 auto;
         margin-right: 20px;
+        text-align: right;
     }
 
 </style>
