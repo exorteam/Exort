c146927c8d1fdce295915aa1f5b3423c3669d1eb
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-07-25 14:17:43 +0800

    delete mongo_test

--- test_for_mongo/src/main/java/exort/testformongo/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package exort.testformongo.service;
-
-
-import org.springframework.stereotype.Service;
-@Service
-public interface TestService {
-    boolean createAssociation(String name,String logo,String description);
-}
