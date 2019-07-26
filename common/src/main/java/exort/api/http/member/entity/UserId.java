485f705115861093f5cae75c035282bf1f18bb68
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-07-26 10:21:44 +0800

    添加UserId类

---
+++ common/src/main/java/exort/api/http/member/entity/UserId.java
@@ -0,0 +1,14 @@
+package exort.api.http.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserId {
+    int userId;
+}
