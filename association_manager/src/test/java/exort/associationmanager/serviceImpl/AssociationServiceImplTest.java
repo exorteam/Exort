9378634beafa0128abac276381632a31fdce0fae
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-07-26 11:03:40 +0800

    Test List  test

--- association_manager/src/test/java/exort/associationmanager/serviceImpl/AssociationServiceImplTest.java
+++ association_manager/src/test/java/exort/associationmanager/serviceImpl/AssociationServiceImplTest.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import exort.api.http.perm.service.PermService;
 import exort.associationmanager.entity.Association;
+import exort.associationmanager.entity.AssociationFilterParams;
 import exort.associationmanager.repository.AssociationRepository;
 import exort.associationmanager.service.AssociationService;
 import org.bson.types.ObjectId;
@@ -53,99 +54,99 @@ public class AssociationServiceImplTest {
     }
 
 
-    @Test
-    public void testCreate() {
-        List<String> ids = new ArrayList<>();
-        for(int i=0;i<ASSOCIATION_NUM;i++){
-            final Association e = associations.get(i);
-            service.createAssociationWithId(e.getId(),e.getName(),e.getDescription(),e.getTags(),e.getLogo());
-            ids.add(e.getId());
-        }
-        for(int i=0;i<ids.size();i++){
-            String Id = ids.get(i);
-            Association contrast = service.getAssociation(Id);
-            Assert.assertEquals(contrast,associations.get(i));
-        }
-
-    }
-
-    @Test
-    public void testGet() {
-        repository.saveAll(associations);
-        for(int i=0;i<ASSOCIATION_NUM;i++){
-            final Association contrast = service.getAssociation(associations.get(i).getId());
-            Assert.assertEquals(associations.get(i),contrast);
-        }
-    }
-//
-    @Test
-    public void testUpdate() {
-        repository.saveAll(associations);
-        for(int i=0;i<ASSOCIATION_NUM;i++){
-            final Association e = associations.get(i);
-            final String title = UUID.randomUUID().toString();
-            final String content = UUID.randomUUID().toString();
-            e.setName(title);
-            e.setDescription(content);
-            e.setLogo(content);
-            e.setTags(Arrays.asList("a","s","d"));
-            service.editAssociation(e.getId(),e.getName(),e.getDescription(),e.getTags(),e.getLogo());
-            associations.set(i,e);
-        }
-        for(int i=0;i<ASSOCIATION_NUM;i++){
-            Association e = associations.get(i);
-            Association contrast = repository.findById(e.getId()).get();
-            Assert.assertEquals(e,contrast);
-        }
-    }
-////
-    @Test
-    public void testBlockAndUnblock() {
-        repository.saveAll(associations);
-        for(int i=0;i<ASSOCIATION_NUM;i++){
-            final String id = associations.get(i).getId();
-            final Association e1 = repository.findById(id).get();
-            Assert.assertEquals(e1.getState(), Integer.valueOf(1));
-
-            service.patchAssociation(id,"block","18+");
-            final Association e2 = repository.findById(id).get();
-            Assert.assertEquals(e2.getState(),Integer.valueOf(0));
-
-            service.patchAssociation(id,"unblock","");
-            final Association e3 = repository.findById(id).get();
-            Assert.assertEquals(e3.getState(),Integer.valueOf(1));
-        }
-    }
-
 //    @Test
-//    public void testListWithFilter() {
-//        Association e = associations.get(0);
-//        String newName = UUID.randomUUID().toString();
-//        e.setName(newName);
-//        String newdesc = UUID.randomUUID().toString();
-//        e.setDescription(newdesc);
-//        List<String> newTags = Arrays.asList("z","x","c");
-//        e.setTags(newTags);
-//        e.setState(-3);
-//        repository.save(e);
+//    public void testCreate() {
+//        List<String> ids = new ArrayList<>();
+//        for(int i=0;i<ASSOCIATION_NUM;i++){
+//            final Association e = associations.get(i);
+//            service.createAssociationWithId(e.getId(),e.getName(),e.getDescription(),e.getTags(),e.getLogo());
+//            ids.add(e.getId());
+//        }
+//        for(int i=0;i<ids.size();i++){
+//            String Id = ids.get(i);
+//            Association contrast = service.getAssociation(Id);
+//            Assert.assertEquals(contrast,associations.get(i));
+//        }
 //
-//        final AssociationFilterParams filter1 = new AssociationFilterParams();
-//        filter1.setKeyword(e.getName());
-//        Assert.assertTrue(service.listAssociations(filter1,0,1).getContent().contains(e));
-//
-//        final AssociationFilterParams filter2 = new AssociationFilterParams();
-//        filter2.setState(-3);
-//        System.out.println(service.listAssociations(filter2,0,700).getContent());
-//        Assert.assertTrue(service.listAssociations(filter2,0,700).getContent().contains(e));
-//
-//        final AssociationFilterParams filter3 = new AssociationFilterParams();
-//        filter3.setKeyword(e.getName());
-//        Assert.assertTrue(service.listAssociations(filter3,0,1).getContent().contains(e));
-//
-//        final AssociationFilterParams filter4 = new AssociationFilterParams();
-//        filter4.setTags(Arrays.asList("x"));
-//        Assert.assertTrue(service.listAssociations(filter4,0,1).getContent().contains(e));
 //    }
+//
+//    @Test
+//    public void testGet() {
+//        repository.saveAll(associations);
+//        for(int i=0;i<ASSOCIATION_NUM;i++){
+//            final Association contrast = service.getAssociation(associations.get(i).getId());
+//            Assert.assertEquals(associations.get(i),contrast);
+//        }
+//    }
+////
+//    @Test
+//    public void testUpdate() {
+//        repository.saveAll(associations);
+//        for(int i=0;i<ASSOCIATION_NUM;i++){
+//            final Association e = associations.get(i);
+//            final String title = UUID.randomUUID().toString();
+//            final String content = UUID.randomUUID().toString();
+//            e.setName(title);
+//            e.setDescription(content);
+//            e.setLogo(content);
+//            e.setTags(Arrays.asList("a","s","d"));
+//            service.editAssociation(e.getId(),e.getName(),e.getDescription(),e.getTags(),e.getLogo());
+//            associations.set(i,e);
+//        }
+//        for(int i=0;i<ASSOCIATION_NUM;i++){
+//            Association e = associations.get(i);
+//            Association contrast = repository.findById(e.getId()).get();
+//            Assert.assertEquals(e,contrast);
+//        }
+//    }
+//////
+//    @Test
+//    public void testBlockAndUnblock() {
+//        repository.saveAll(associations);
+//        for(int i=0;i<ASSOCIATION_NUM;i++){
+//            final String id = associations.get(i).getId();
+//            final Association e1 = repository.findById(id).get();
+//            Assert.assertEquals(e1.getState(), Integer.valueOf(1));
+//
+//            service.patchAssociation(id,"block","18+");
+//            final Association e2 = repository.findById(id).get();
+//            Assert.assertEquals(e2.getState(),Integer.valueOf(0));
+//
+//            service.patchAssociation(id,"unblock","");
+//            final Association e3 = repository.findById(id).get();
+//            Assert.assertEquals(e3.getState(),Integer.valueOf(1));
+//        }
+//    }
+
+    @Test
+    public void testListWithFilter() {
+        Association e = associations.get(0);
+        String newName = UUID.randomUUID().toString();
+        e.setName(newName);
+        String newdesc = UUID.randomUUID().toString();
+        e.setDescription(newdesc);
+        List<String> newTags = Arrays.asList("z","x","c");
+        e.setTags(newTags);
+        e.setState(-3);
+        repository.save(e);
+
+        final AssociationFilterParams filter1 = new AssociationFilterParams();
+        filter1.setKeyword(e.getName());
+        Assert.assertTrue(service.listAssociations(filter1,0,1).getContent().contains(e));
+
+        final AssociationFilterParams filter2 = new AssociationFilterParams();
+        filter2.setState(-3);
+        System.out.println(service.listAssociations(filter2,0,700).getContent());
+        Assert.assertTrue(service.listAssociations(filter2,0,700).getContent().contains(e));
+
+        final AssociationFilterParams filter3 = new AssociationFilterParams();
+        filter3.setKeyword(e.getName());
+        Assert.assertTrue(service.listAssociations(filter3,0,1).getContent().contains(e));
+
+        final AssociationFilterParams filter4 = new AssociationFilterParams();
+        filter4.setTags(Arrays.asList("x"));
+        Assert.assertTrue(service.listAssociations(filter4,0,1).getContent().contains(e));
+    }
 //    @Test
 //    public void TestHandleApplication(){
 //        Association e = associations.get(0);
