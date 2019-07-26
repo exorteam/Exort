e22dee9406164e7ea4121fe6aaf69f6232c04450
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-07-26 16:34:45 +0800

    从git仓库中移除配置文件；在CI中导入不同的配置文件

--- association_member_manager/src/test/java/exort/association_member_manager/serviceimpl/AssociationMemberManageServiceImplTest.java
+++ association_member_manager/src/test/java/exort/association_member_manager/serviceimpl/AssociationMemberManageServiceImplTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
@@ -22,6 +23,7 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class AssociationMemberManageServiceImplTest {
     @Autowired
     private AssociationMemberManageService associationMemberManageService;
