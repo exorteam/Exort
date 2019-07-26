052c795fb5095c1748480d0a77f7bf9783c02b52
Ice Cola <icecola2018@gmail.com>
2019-07-26 10:46:12 +0800

    ApiErrorHandler中加上了正确的ErrorController

--- common/src/main/java/exort/api/http/common/errorhandler/ApiErrorHandler.java
+++ common/src/main/java/exort/api/http/common/errorhandler/ApiErrorHandler.java
@@ -2,6 +2,7 @@ package exort.api.http.common.errorhandler;
 
 import exort.api.http.common.entity.ApiResponse;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -14,7 +15,8 @@ import org.springframework.validation.BindException;
 import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
-public class ApiErrorHandler {
+@RestController
+public class ApiErrorHandler implements ErrorController {
 
     @ExceptionHandler({ApiError.class})
     public ResponseEntity<ApiResponse> handleApiError(ApiError e, WebRequest request) {
@@ -45,6 +47,28 @@ public class ApiErrorHandler {
     public ResponseEntity<ApiResponse> handleOtherError(Exception e, WebRequest request) {
         e.printStackTrace();
         return new ResponseEntity<>(new ApiResponse("internal_server_error", e.getMessage()), HttpStatus.valueOf(500));
+    }
+
+    @RequestMapping("/error")
+    public String handleHandlerNotFoundException(HttpServletRequest request) {
+        Integer code = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        System.err.println("error: " + code);
+        if (code != null && code == 404) {
+            throw new ApiError(404, "resource_not_found", "No resource matched by URL. Find out the right url through our documents.");
+        } else {
+            Exception e = (Exception) request.getAttribute("javax.servlet.error.exception");
+            String message = "unknown error";
+            if (e != null) {
+                message = e.getMessage();
+                e.printStackTrace();
+            }
+            throw new ApiError(500, "internal_server_error", message);
+        }
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 
 }
