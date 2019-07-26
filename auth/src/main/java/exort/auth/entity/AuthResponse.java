1adf9b7cbab2e03315e01bfb849be3847c9695c3
rsp <1747836307@qq.com>
2019-07-26 09:16:01 +0800

    delete auth files for pull request

--- auth/src/main/java/exort/auth/entity/AuthResponse.java
+++
@@ -1,9 +0,0 @@
-package exort.auth.entity;
-
-import lombok.Data;
-
-@Data
-public class AuthResponse {
-	private int    id;
-	private String username;
-}
--- auth/src/main/java/exort/auth/repository/InfoRepository.java
+++
@@ -1,9 +0,0 @@
-package exort.auth.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import exort.auth.entity.UserInfo;
-
-public interface InfoRepository extends MongoRepository<UserInfo,Integer> {
-
-}
