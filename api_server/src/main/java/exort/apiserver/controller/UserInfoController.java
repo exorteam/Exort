cd3ccddd64d674942059e74126c3aeae960dfcfa
MrHate <dgyhateyou@sjtu.edu.cn>
2019-07-26 10:26:19 +0800

    correct rest path and add jwt token auth for UserInfoController

--- api_server/src/main/java/exort/apiserver/controller/UserInfoController.java
+++ api_server/src/main/java/exort/apiserver/controller/UserInfoController.java
@@ -3,6 +3,7 @@ package exort.apiserver.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,26 +13,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import exort.apiserver.service.UserInfoService;
 import exort.apiserver.service.UserInfoService.UserInfo;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
+@Log4j2
 @RequestMapping(path="/users")
 public class UserInfoController {
 
 	@Autowired
-	private UserInfoService service;
+	private UserInfoService infoSvc;
 
-	@GetMapping("/info")
-	public UserInfo getUserInfoById(@RequestAttribute("id") int id){
-		return service.getUserInfo(id);
+	@GetMapping("/{id}")
+	public UserInfo getUserInfoById(@RequestAttribute("id") int operatorId,@PathVariable("id") int userId){
+		log.info("Operator("+String.valueOf(operatorId)+") get user info of user("+String.valueOf(userId)+").");
+		return infoSvc.getUserInfo(userId);
 	}
 
-	@PostMapping("/info")
-	public UserInfo updateUserInfoById(@RequestAttribute("id") int id,@RequestBody UserInfo info){
-		return service.updateUserInfo(id,info);
+	@GetMapping("/self")
+	public UserInfo getCurrentUserInfo(@RequestAttribute("id") int operatorId){
+		log.info("Operator("+String.valueOf(operatorId)+") get current user info.");
+		return infoSvc.getUserInfo(operatorId);
 	}
 
-	@PatchMapping("/info")
-	public boolean disableUserById(@RequestAttribute("id") int id,@RequestParam boolean disabled){
-		return service.disableUser(id,disabled);
+	@PostMapping("/{id}")
+	public UserInfo updateUserInfoById(@RequestAttribute("id") int operatorId,@PathVariable("id") int userId,@RequestBody UserInfo info){
+		log.info("Operator("+String.valueOf(operatorId)+") update user info of user("+String.valueOf(userId)+").");
+		return infoSvc.updateUserInfo(userId,info);
+	}
+
+	@PatchMapping("/{id}")
+	public boolean disableUserById(@RequestAttribute("id") int operatorId,@PathVariable("id") int userId,@RequestParam boolean disabled){
+		log.info("Operator("+String.valueOf(operatorId)+") disable user("+String.valueOf(userId)+").");
+		return infoSvc.disableUser(userId,disabled);
 	}
 }
