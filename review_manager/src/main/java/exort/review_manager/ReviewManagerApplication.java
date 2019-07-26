a45af7755114976e507c1f7e94ea6438ac6e4104
Ice Cola <icecola2018@gmail.com>
2019-07-27 00:13:39 +0800

    初始化review_manager

---
+++ review_manager/src/main/java/exort/review_manager/ReviewManagerApplication.java
@@ -0,0 +1,13 @@
+package exort.review_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewManagerApplication.class, args);
+    }
+
+}
---
+++ review_manager/src/test/java/exort/review_manager/ReviewManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package exort.review_manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
