a2c2b15d65bd08bda679b4493fff98e2a5ccce0e
rsp <1747836307@qq.com>
2019-07-26 16:44:42 +0800

    从git中移除了配置文件；在CI中导入不同环境的配置

--- activity_manager/src/test/java/exort/activity/serviceImpl/ActivityServiceImplTest.java
+++ activity_manager/src/test/java/exort/activity/serviceImpl/ActivityServiceImplTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class ActivityServiceImplTest {
 
     @Autowired
