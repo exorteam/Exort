17bbc7fdc2b25cbda23f142c3fabe4c10a939878
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-07-29 16:15:03 +0800

    更改为引用common

--- api_server/src/main/java/exort/apiserver/ApiserverApplication.java
+++ api_server/src/main/java/exort/apiserver/ApiserverApplication.java
@@ -1,5 +1,6 @@
 package exort.apiserver;
 
+import exort.api.http.member.service.AssoMemServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -7,11 +8,10 @@ import org.springframework.context.annotation.Import;
 import exort.api.http.perm.service.PermServiceImpl;
 
 @SpringBootApplication
-@Import({PermServiceImpl.class})
-public class ApiserverApplication {
+@Import({PermServiceImpl.class, AssoMemServiceImpl.class})
 
+public class ApiserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiserverApplication.class, args);
 	}
-
 }
