286d4d359df31ea795ae31ec1b6df8b169a34349
MrHate <dgyhateyou@sjtu.edu.cn>
2019-07-29 10:37:15 +0800

    add path to filter

--- api_server/src/main/java/exort/apiserver/config/JwtConfig.java
+++ api_server/src/main/java/exort/apiserver/config/JwtConfig.java
@@ -17,7 +17,9 @@ public class JwtConfig {
 		registrationBean.setFilter(new JwtFilter());
 		registrationBean.addUrlPatterns("/users/*");
 		registrationBean.addUrlPatterns("/test/auth");
-		registrationBean.addUrlPatterns("/perm/*");
+		registrationBean.addUrlPatterns("/permission/*");
+		registrationBean.addUrlPatterns("/activities/*");
+		registrationBean.addUrlPatterns("/associations/*");
 
 		return registrationBean;
 	}
