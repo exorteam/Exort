715303ffd6ced6447fc6f81203944b96e5daa6ec
MrHate <dgyhateyou@sjtu.edu.cn>
2019-07-31 15:40:21 +0800

    add permission service to article controller

--- api_server/src/main/java/exort/apiserver/controller/ArticleController.java
+++ api_server/src/main/java/exort/apiserver/controller/ArticleController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import exort.api.http.common.entity.ApiResponse;
+import exort.api.http.perm.service.PermService;
 import exort.apiserver.service.ArticleService;
 import exort.apiserver.service.ArticleService.Article;
 import exort.apiserver.service.ArticleService.ArticleFilterParam;
@@ -23,9 +25,11 @@ public class ArticleController {
 
 	@Autowired
 	private ArticleService articleSvc;
+	@Autowired
+	private PermService permSvc;
 
 	@PostMapping
-	public ApiResponse createArticle(@RequestBody Article e){
+	public ApiResponse createArticle(@RequestAttribute("id") long operatorId,@RequestBody Article e){
 		return articleSvc.createArticle(e);
 	}
 
@@ -35,12 +39,12 @@ public class ArticleController {
 	}
 
 	@DeleteMapping("/{id}")
-	public ApiResponse deleteArticle(@PathVariable("id") int articleId){
+	public ApiResponse deleteArticle(@RequestAttribute("id") long operatorId,@PathVariable("id") int articleId){
 		return articleSvc.deleteArticle(articleId);
 	}
 
 	@PutMapping("/{id}")
-	public ApiResponse updateArticle(@PathVariable("id") int articleId,@RequestBody Article e){
+	public ApiResponse updateArticle(@RequestAttribute("id") long operatorId,@PathVariable("id") int articleId,@RequestBody Article e){
 		return articleSvc.updateArticle(articleId,e);
 	}
 
@@ -50,7 +54,7 @@ public class ArticleController {
 	}
 
 	@PatchMapping("/{id}")
-	public ApiResponse publishArticle(@PathVariable("id") int articleId,@RequestParam boolean publish){
+	public ApiResponse publishArticle(@RequestAttribute("id") long operatorId,@PathVariable("id") int articleId,@RequestParam boolean publish){
 		return articleSvc.publishArticle(articleId,publish);
 	}
 
