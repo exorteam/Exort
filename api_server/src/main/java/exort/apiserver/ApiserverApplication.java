fbb80eca4376795a4bea7afe0a743cb1028113c9
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-07-31 09:44:32 +0800

    Update ApiserverApplication.java

--- api_server/src/main/java/exort/apiserver/ApiserverApplication.java
+++ api_server/src/main/java/exort/apiserver/ApiserverApplication.java
@@ -2,6 +2,7 @@ package exort.apiserver;
 
 import exort.api.http.activity.service.ActivityServiceImpl;
 import exort.api.http.member.service.AssoMemServiceImpl;
+import exort.apiserver.service.impl.AssociationServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Import;
 import exort.api.http.perm.service.PermServiceImpl;
 
 @SpringBootApplication
-@Import({PermServiceImpl.class, AssoMemServiceImpl.class, ActivityServiceImpl.class})
+@Import({PermServiceImpl.class, AssoMemServiceImpl.class, ActivityServiceImpl.class, AssociationServiceImpl.class})
 public class ApiserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiserverApplication.class, args);
