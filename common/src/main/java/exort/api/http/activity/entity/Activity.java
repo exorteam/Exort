b2033ab0d63884633281b969f72e0d29a850e0eb
rsp <1747836307@qq.com>
2019-07-31 11:18:17 +0800

    Update Activity.java

--- common/src/main/java/exort/api/http/activity/entity/Activity.java
+++ common/src/main/java/exort/api/http/activity/entity/Activity.java
@@ -35,9 +35,7 @@ public class Activity {
     private String image;
 
     public Activity(List<String> associationIds, ActivityTime signupTime, ActivityTime time, String title, String content,
-                    int publishState, int signupState, int state, boolean ifReview, boolean ifOnlyMem, int maxParticipants,
-                    List<Integer> materialTemplateIds, List<Integer> participantIds, List<Integer> realParticipantIds,
-                    List<String> tags, String image) {
+                     boolean ifReview, boolean ifOnlyMem, int maxParticipants, List<Integer> materialTemplateIds, List<String> tags, String image) {
         this.associationIds = associationIds;
         this.createTime = new Date();
         this.publishTime = new Date();
@@ -47,15 +45,10 @@ public class Activity {
         this.time = time;
         this.title = title;
         this.content = content;
-        this.publishState = publishState;
-        this.signupState = signupState;
-        this.state = state;
         this.ifReview = ifReview;
         this.ifOnlyMem = ifOnlyMem;
         this.maxParticipants = maxParticipants;
         this.materialTemplateIds = materialTemplateIds;
-        this.participantIds = participantIds;
-        this.realParticipantIds = realParticipantIds;
         this.tags = tags;
         this.image = image;
     }
