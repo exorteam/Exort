c56ff33622870e0e1016154b328309ee4079b47b
MrHate <dgyhateyou@sjtu.edu.cn>
2019-07-31 10:11:35 +0800

    rest interfaces return with ApiResponse wrapped

--- article_manager/src/main/java/exort/articlemanager/controller/ArticleController.java
+++ article_manager/src/main/java/exort/articlemanager/controller/ArticleController.java
@@ -1,7 +1,7 @@
 package exort.articlemanager.controller;
 
-import java.util.List;
 import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import exort.api.http.common.entity.ApiResponse;
+import exort.api.http.common.errorhandler.ApiError;
 import exort.articlemanager.entity.Article;
 import exort.articlemanager.entity.ArticleFilterParams;
 import exort.articlemanager.service.ArticleService;
@@ -26,41 +28,68 @@ public class ArticleController {
 	private ArticleService service;
 
 	@PostMapping
-	public Article createArticle(@RequestBody Article article){
-		return service.createArticle(article);
+	public ApiResponse createArticle(@RequestBody Article article){
+		Article res = service.createArticle(article);
+		if(res == null){
+			throw new ApiError(404,"CreationErr","Error occured when creating article");
+		}
+		return new ApiResponse<Article>(res);
 	}
 
 	@DeleteMapping("/{id}")
-	public boolean deleteArticle(@PathVariable("id") int articleId){
-		return service.deleteArticle(articleId);
+	public ApiResponse deleteArticle(@PathVariable("id") int articleId){
+		boolean res = service.deleteArticle(articleId);
+		if(!res){
+			throw new ApiError(404,"DeletionErr","Error occured when deleting article["+String.valueOf(articleId)+"]");
+		}
+		return new ApiResponse<Boolean>(Boolean.valueOf(res));
 	}
 
 	@PutMapping("/{id}")
-	public boolean updateArticle(@PathVariable("id") int articleId, @RequestBody Map<String,String> body){
+	public ApiResponse updateArticle(@PathVariable("id") int articleId, @RequestBody Map<String,String> body){
 		String title = body.get("title");
 		String content = body.get("content");
-		if(title == null || content == null)return false;
-		return service.updateArticle(articleId,title,content);
+		if(title == null || content == null){
+			return new ApiResponse<Boolean>(Boolean.FALSE);
+		}
+		boolean res = service.updateArticle(articleId,title,content);
+		if(!res){
+			throw new ApiError(404,"UpdateErr","Error occured when updating article["+String.valueOf(articleId)+"]");
+		}
+		return new ApiResponse<Boolean>(Boolean.valueOf(res));
 	}
 
 	@GetMapping("/{id}")
-	public Article getArticle(@PathVariable("id") int articleId){
-		return service.getArticle(articleId);
+	public ApiResponse getArticle(@PathVariable("id") int articleId){
+		Article res = service.getArticle(articleId);
+		if(res == null){
+			throw new ApiError(404,"GetErr","Error occured when getting article["+String.valueOf(articleId)+"]");
+		}
+		return new ApiResponse<Article>(res);
 	}
 
 	@GetMapping
-	public List<Article> listArticle(@RequestBody ArticleFilterParams params){
-		return service.listArticle(params);
+	public ApiResponse listArticle(@RequestBody ArticleFilterParams params){
+		List<?> res = service.listArticle(params);
+		if(res == null){
+			throw new ApiError(404,"GetErr","Error occured when listing article");
+		}
+		return new ApiResponse<List>(res);
 	}
 
 	@PatchMapping("/{id}")
-	public boolean publishArticle(@PathVariable("id") int articleId,@RequestParam boolean publish){
+	public ApiResponse publishArticle(@PathVariable("id") int articleId,@RequestParam boolean publish){
+		boolean res = false;
 		if(publish){
-			return service.publishArticle(articleId);
+			res = service.publishArticle(articleId);
 		}
 		else{
-			return service.withdrawArticle(articleId);
+			res = service.withdrawArticle(articleId);
 		}
+		if(!res){
+			throw new ApiError(404,"UpdateErr","Error occured when updating article["+String.valueOf(articleId)+"]");
+		}
+		return new ApiResponse<Boolean>(Boolean.valueOf(res));
 	}
 
 }
