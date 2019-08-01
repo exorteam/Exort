385ad41b6217a137f44628a7b254e46cf1ac29ec
rsp <1747836307@qq.com>
2019-08-01 17:20:38 +0800

    update api service part of activity

--- common/src/main/java/exort/api/http/activity/service/ActivityService.java
+++ common/src/main/java/exort/api/http/activity/service/ActivityService.java
@@ -1,33 +1,35 @@
-package exort.api.http.activity.service;
+package exort.apiserver.service;
 
-import exort.api.http.activity.entity.*;
+import exort.api.http.activity.entity.Activity;
+import exort.api.http.activity.entity.Filter;
+import exort.api.http.activity.entity.RequestActivity;
+import exort.api.http.activity.entity.Signup;
 import exort.api.http.common.entity.ApiResponse;
 import exort.api.http.common.entity.PageQuery;
 import exort.api.http.common.entity.PagedData;
 import exort.api.http.review.entity.CallbackParam;
 
-public interface ActivityService {
+public interface ActivityManagerService {
 
     public ApiResponse<Activity> createNewActivity(Activity activity);
 
-    public ApiResponse<Activity> updateActivity(Activity activity, String activityId);
+    public ApiResponse<Activity> updateActivity(Activity activity, String activityid);
 
-    public ApiResponse<PagedData<Activity>> getActivities(Filter filter, PageQuery pageQuery);
+    public ApiResponse<PagedData<Activity>> getActivities(Filter select, PageQuery pageQuery);
 
-    public ApiResponse<Object> publishActivity(String activityId, RequestActivity requestActivity);
+    public ApiResponse<Object> publishActivity(String activityid, RequestActivity requestActivity);
 
-    public ApiResponse<Object> addParticipants(String activityId, RequestActivity requestActivity);
+    public ApiResponse<Object> addParticipants(String activityid, RequestActivity requestActivity);
 
-    public ApiResponse<Object> addRealParticipants(String activityId, RequestActivity requestActivity);
+    public ApiResponse<Object> addRealParticipants(String activityid, RequestActivity requestActivity);
 
-    public ApiResponse<Object> deleteParticipants(String activityId, RequestActivity requestActivity);
+    public ApiResponse<Object> deleteParticipants(String activityid, RequestActivity requestActivity);
 
-    public ApiResponse<PagedData<Integer>> getActivityParticipants(PageQuery pageQuery, String activityId, RequestActivity requestActivity);
+    public ApiResponse<PagedData<Integer>> getActivityParticipants(PageQuery pageQuery, String activityid, RequestActivity requestActivity);
 
-    public ApiResponse<PagedData<Integer>> getActivityRealParticipants(PageQuery pageQuery, String activityId, RequestActivity requestActivity);
+    public ApiResponse<PagedData<Integer>> getActivityRealParticipants(PageQuery pageQuery, String activityid, RequestActivity requestActivity);
 
     public ApiResponse<Object> acceptSignup(CallbackParam<Signup> operation);
 
-    public ApiResponse<Activity> getActivity(String activityId);
-
+    public ApiResponse<Activity> getActivity(String activityid);
 }
