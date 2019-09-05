de36d2f063c3a76f882ccc3e42e9778993dc0a42
Ice Cola <icecola2018@gmail.com>
2019-09-05 22:02:19 +0800

    给AssociationMemberSignUp加上NoArgsConstructor

--- common/src/main/java/exort/api/http/review/entity/details/AssociationMemberSignUp.java
+++ common/src/main/java/exort/api/http/review/entity/details/AssociationMemberSignUp.java
@@ -2,9 +2,11 @@ package exort.api.http.review.entity.details;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AssociationMemberSignUp {
 
     String associationId;
