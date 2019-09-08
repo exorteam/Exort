ac511ccd4652143034d6a96ce579928ba336c4c3
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-09-08 23:45:31 +0800

    修改testfile

--- comment/src/test/java/exort/comment/service/impl/CommentServiceImplementTest.java
+++ comment/src/test/java/exort/comment/service/impl/CommentServiceImplementTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Calendar;
@@ -18,6 +19,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class CommentServiceImplementTest {
     @Autowired
     private CommentService commentService;
